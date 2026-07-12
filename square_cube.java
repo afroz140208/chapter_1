6ebcc1694b96de017431982759a10f7cff1c7861
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:04:03 +0530

    Add files via upload

---
+++ square_cube.java
@@ -0,0 +1,15 @@
+package ch2;
+
+public class square_cube {
+
+	public static void main(String[] args) {
+		int num=6;
+		int square=num*num;
+		System.out.println("square of"+num+"is"+square);
+		int cube=num*num*num;
+		System.out.println("cube of"+num+"is"+cube);
+		
+
+	}
+
+}
