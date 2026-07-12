4e4fd2339ce0ec3c546dd6d7e5831c38db10f20a
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:04:17 +0530

    Add files via upload

---
+++ student_details.java
@@ -0,0 +1,17 @@
+package ch2;
+
+public class student_details {
+
+	public static void main(String[] args) {
+		String name ="abi";
+		int num = 70;
+		System.out.println("student name :"+name);
+		System.out.println("student mark :"+num);
+		
+		
+		
+		
+
+	}
+
+}
