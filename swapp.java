b1a1dd52c05d35c8ba77ccda7d77cea2305d1c69
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:04:48 +0530

    Add files via upload

---
+++ swapp.java
@@ -0,0 +1,16 @@
+package ch2;
+
+public class swapp {
+
+	public static void main(String[] args) {
+		int a = 8;
+		int b = 10;
+		a = a*b;
+		b = a/b;
+		a = a/b;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+
+	}
+
+}
