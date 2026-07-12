71ec796cd11cb54955e340c77ceb8c27b48b6d39
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:02:20 +0530

    Add files via upload

---
+++ largest_of_3nos.java
@@ -0,0 +1,14 @@
+package ch2;
+
+public class largest_of_3nos {
+
+	public static void main(String[] args) {
+		double a=450;
+		double b=500;
+		double c=200;
+		System.out.println("largest of three:"+ Math.max(a,Math.max(b, c)));
+		
+
+	}
+
+}
