8fa7fd0bd89f61c4b621efa6b06081f40285b95d
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:02:02 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,14 @@
+package ch2;
+
+public class largest {
+
+	public static void main(String[] args) {
+		int a=170;
+		int b=200;
+		System.out.println("largest number:"+ Math.max(a,b));
+	
+	
+
+	}
+
+}
