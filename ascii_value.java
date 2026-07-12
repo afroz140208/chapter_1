28bd9df5c6b68ade6408380a2c2003aaf56f5f75
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:01:07 +0530

    Add files via upload

---
+++ ascii_value.java
@@ -0,0 +1,17 @@
+package ch2;
+
+public class ascii_value {
+
+	public static void main(String[] args) {
+		char ch1='d';
+		char ch2='r';
+		int a = ch1;
+		int b = ch2;
+	System.out.println("Ascii value of"+ch1+"is"+a);
+	System.out.println("Ascii value of"+ch2+"is"+b);
+	
+		
+
+	}
+
+}
