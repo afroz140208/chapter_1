26e1a2e419f6bf55d8f52140ca5afa2ef71249bf
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:03:46 +0530

    Add files via upload

---
+++ simple_interest.java
@@ -0,0 +1,17 @@
+package ch2;
+
+public class simple_interest {
+
+	public static void main(String[] args) {
+		double p = 6000;
+		double r = 5.5;
+		double t = 2;
+		double si=(p*r*t)/100;
+		System.out.println("simple interest:"+si);
+		
+		
+			
+
+	}
+
+}
