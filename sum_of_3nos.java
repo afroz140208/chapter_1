67d1145f3fa8537bcf5862094fc6c3edef9d3fbe
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:04:34 +0530

    Add files via upload

---
+++ sum_of_3nos.java
@@ -0,0 +1,14 @@
+package ch2;
+
+public class sum_of_3nos {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		int z = 5;
+		int sum = x+y+z;
+		System.out.println("sum:"+sum);
+
+	}
+
+}
