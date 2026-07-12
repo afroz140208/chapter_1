bf21d16233e37e975acd25c5fd844e19880e6ce5
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:02:56 +0530

    Add files via upload

---
+++ odd_or_even.java
@@ -0,0 +1,14 @@
+package ch2;
+
+public class odd_or_even {
+
+	public static void main(String[] args) {
+		int number = 6;
+		if (number % 2 == 0 )
+			System.out.println("it is even number");
+			
+			
+
+	}
+
+}
