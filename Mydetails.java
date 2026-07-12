bb31727dbfa8f6e9a8c7f272aeb9db1c3a81255b
afroz140208 <afroz.elisa14@gmail.com>
2026-07-12 15:02:34 +0530

    Add files via upload

---
+++ Mydetails.java
@@ -0,0 +1,12 @@
+package ch2;
+
+public class Mydetails {
+
+	public static void main(String[] args) {
+		System.out.println("My Name is Afroz");
+		System.out.println("My age is 18");
+    	System.out.println("My favourite subject is eng");
+
+	}
+
+}
